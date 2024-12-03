82388c4e8006d8e8631dc48fcadb792df9b21429
wildson018 <wildson117@gmail.com>
2024-12-02 21:28:21 -0400

    finalização e correção de bugs

--- Trabalho Mariela/init.py
+++ Trabalho Mariela/init.py
@@ -10,6 +10,27 @@ usuarios = {
 def contato():
     return render_template("contato.html")
 
+@app_Wildson.route("/cadastro", methods=["GET", "POST"])
+def cadastro():
+    if request.method == "POST":
+        name = request.form.get("name")
+        cpf = request.form.get("cpf")
+        email = request.form.get("email")
+        phone = request.form.get("phone")
+        address = request.form.get("address")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm_password")
+
+       
+        if password != confirm_password:
+            flash("As senhas não coincidem. Tente novamente!", "error")
+            return redirect(url_for("register"))
+
+       
+        flash("Cadastro realizado com sucesso!", "success")
+
+    return render_template("cadastro.html")
+
 @app_Wildson.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
