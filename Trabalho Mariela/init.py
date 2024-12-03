6d424d8adab51559012e27ac00d901f93c8058bb
wildson018 <wildson117@gmail.com>
2024-12-02 20:13:42 -0400

    backend login

--- Trabalho Mariela/init.py
+++ Trabalho Mariela/init.py
@@ -7,29 +7,6 @@ app_Wildson.secret_key = "secretkey"  # Necessário para mensagens flash
 def login():
     return render_template("login.html")
 
-@app_Wildson.route("/cadastro", methods=["GET", "POST"])
-
-
-@app_Wildson.route("/register", methods=["GET", "POST"])
-def cadastro():
-    if request.method == "POST":
-        name = request.form.get("name")
-        cpf = request.form.get("cpf")
-        email = request.form.get("email")
-        phone = request.form.get("phone")
-        address = request.form.get("address")
-        password = request.form.get("password")
-        confirm_password = request.form.get("confirm_password")
-
-       
-        if password != confirm_password:
-            flash("As senhas não coincidem. Tente novamente!", "error")
-            return redirect(url_for("register"))
-
-       
-        flash("Cadastro realizado com sucesso!", "success")
-
-    return render_template("cadastro.html")
 
 if __name__ == "__main__":
     app_Wildson.run(debug=True)
